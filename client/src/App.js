2ee2fd5688fbddcd2a75d34f9037dadd6e3fd9ae
Nguyễn Đình Trường <53716352+ndtr2000@users.noreply.github.com>
2021-11-11 17:33:43 +0700

    init project

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import TopBar from './components/topbar/TopBar';
+
+function App() {
+  return (
+    <>
+      <TopBar/>
+      <p>test</p>
+
+    </>
+  );
+}
+
+export default App;
